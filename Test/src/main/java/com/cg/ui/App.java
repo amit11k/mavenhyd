4805fdc1ca18cbd5ad93b3a47e824e4f0b225cd7
Amit Kalyan <Administrator@DIN86000504.corp.capgemini.com>
2017-09-18 11:07:14 +0530

    First Maven

---
+++ Test/src/main/java/com/cg/ui/App.java
@@ -0,0 +1,11 @@
+package com.cg.ui;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+	MyTest m = new MyTest();
+	m.getData();
+        System.out.println( "Hello Maven" );
+    }
+}
---
+++ Test/src/main/java/com/cg/ui/MyTest.java
@@ -0,0 +1,11 @@
+package com.cg.ui;
+
+class MyTest{
+
+public void getData(){
+
+System.out.println("Hello Capgemini");
+
+}
+
+}
